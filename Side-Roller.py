b0c06ecdca48583dc2320dcc44d6ff5f6f82178e
Stephen McArthur <invoiceme@hotmail.com>
2013-06-21 23:58:08 -0400

    V0.3

--- Side-Roller.py
+++ Side-Roller.py
@@ -3,7 +3,7 @@
 # Assinment 4
 # Side ROLLER 
 
-import pygame, random
+import pygame, random, time
 pygame.init()
 
 screen = pygame.display.set_mode((800, 600))
@@ -25,13 +25,23 @@ class Roller(pygame.sprite.Sprite):
         self.image = pygame.image.load("roller.png")
         self.rect = self.image.get_rect()
         self.image.set_colorkey(white)
+
+        if not pygame.mixer:
+            print("problem with sound")
+        else:
+            pygame.mixer.init()
+            self.soundWaka = pygame.mixer.Sound("waka.wav")
+            self.soundKill = pygame.mixer.Sound("eatghost.wav")
+            self.soundDeath = pygame.mixer.Sound("death.wav")
+            self.soundBackground = pygame.mixer.Sound("cursed_dream.wav")
+            self.soundBackground.play(-1)
         
     def update(self):
         mousex, mousey = pygame.mouse.get_pos()
         self.rect.center = (mousex, 500)
         
         
-class Enemy(pygame.sprite.Sprite):
+class redEnemy(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
         self.image = pygame.image.load("enemy_red.png")
@@ -40,7 +50,7 @@ class Enemy(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.reset()
         
-        self.dx = 2
+        self.dx = 3
     
     def update(self):
         self.rect.centerx -= self.dx
@@ -50,18 +60,76 @@ class Enemy(pygame.sprite.Sprite):
     def reset(self):
         self.rect.top = 470
         self.rect.centerx = 860
+
+class peachEnemy(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load("enemy_peach.png")
+        self.image = self.image.convert()
+        self.image.set_colorkey(white)
+        self.rect = self.image.get_rect()
+        self.reset()
+        
+        self.dx = 3
     
+    def update(self):
+        self.rect.centerx -= self.dx
+        if self.rect.right < 0:
+            self.reset()
+            
+    def reset(self):
+        self.rect.top = 470
+        self.rect.centerx = 920
+ 
+class whiteEnemy(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load("enemy_white.png")
+        self.image = self.image.convert()
+        self.image.set_colorkey(white)
+        self.rect = self.image.get_rect()
+        self.reset()
+        
+        self.dx = 3
+    
+    def update(self):
+        self.rect.centerx -= self.dx
+        if self.rect.right < 0:
+            self.reset()
+            
+    def reset(self):
+        self.rect.top = 470
+        self.rect.centerx = 980
+ 
+class Background(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load("gameBackground.png")
+        self.image = self.image.convert()
+        self.rect = self.image.get_rect()
+        self.dy = 2
+        self.reset()
+        
+    def update(self):
+        self.rect.left -= self.dy
+        if self.rect.right <= 800:
+            self.reset() 
+    
+    def reset(self):
+        self.rect.left = 0 
     
 def main():
     screen = pygame.display.set_mode((800, 600))
-    pygame.display.set_caption("Mail Pilot! mpIsland - adding the Island")
+    pygame.display.set_caption("Side ROLLER!")
 
-    background = pygame.image.load("gameBackground.png").convert()
-    screen.blit(background, (0, 0))
     roller = Roller()
-    enemy = Enemy()
+    enemyR = redEnemy()
+    enemyP = peachEnemy()
+    enemyW = whiteEnemy()
+    background = Background()
     
-    allSprites = pygame.sprite.Group(enemy, roller)
+    goodSprites = pygame.sprite.Group(background, roller)
+    badSprites = pygame.sprite.Group(enemyR, enemyP, enemyW)
     clock = pygame.time.Clock()
     keepGoing = True
     while keepGoing:
@@ -70,13 +138,24 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 keepGoing = False
-                
-        allSprites.clear(screen, background)
-        allSprites.update()
-        allSprites.draw(screen)
+        
+        #check collisions
+        hitEnemys = pygame.sprite.spritecollide(roller, badSprites, False)
+        
+        if hitEnemys:
+            roller.soundDeath.play()
+            for theEnemy in hitEnemys:
+                theEnemy.reset()
+        
+        goodSprites.update()
+        badSprites.update()
+        goodSprites.draw(screen)
+        badSprites.draw(screen)
         
         pygame.display.flip()
     
+    roller.soundBackground.stop()
+
     #return mouse cursor
     pygame.mouse.set_visible(True) 
 if __name__ == "__main__":
