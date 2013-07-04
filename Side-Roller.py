daad4c9135472f7d9783d07a5ae4e0c847dd16cd
Stephen McArthur <invoiceme@hotmail.com>
2013-07-04 14:05:44 -0400

    V0.7

--- Side-Roller.py
+++ Side-Roller.py
@@ -3,7 +3,7 @@
 # Assinment 4
 # Side ROLLER 
 
-import pygame, random, time
+import pygame, random, time, math
 pygame.init()
 
 screen = pygame.display.set_mode((800, 600))
@@ -35,7 +35,7 @@ class Roller(pygame.sprite.Sprite):
             self.soundKill = pygame.mixer.Sound("eatghost.wav")
             self.soundDeath = pygame.mixer.Sound("death.wav")
             self.soundBackground = pygame.mixer.Sound("cursed_dream.wav")
-            self.soundBackground.play(-1)
+            self.soundIntro = pygame.mixer.Sound("chemicalPlant.wav")
         
     def update(self):
         mousex, mousey = pygame.mouse.get_pos()
@@ -51,7 +51,7 @@ class Bullet(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.reset()
         
-        self.dx = 5
+        self.dx = 6
         self.shoot = False
 	
     def update(self):
@@ -131,6 +131,35 @@ class whiteEnemy(pygame.sprite.Sprite):
         self.rect.top = 450
         self.rect.centerx = random.randrange(800, 1500)
 
+class Obstical(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load("fallingBall.png")
+        self.image = self.image.convert()
+        self.image.set_colorkey(white)
+        self.rect = self.image.get_rect()
+        self.reset()
+        
+        self.dy = 3
+        self.attack = False
+	
+    def update(self):
+        
+        if self.rect.top > screen.get_height():
+            #self.attack = False
+            self.reset()
+		
+        if self.attack == True:
+            self.rect.centery += self.dy
+
+        if self.attack == False:
+            self.rect.centery = -10
+            self.rect.centerx = 400
+			
+    def reset(self):
+        self.rect.bottom = 0
+        self.rect.centerx = random.randrange(0, screen.get_width()) 
+		
 class Scoreboard(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
@@ -163,6 +192,7 @@ class Background(pygame.sprite.Sprite):
 def game():
     screen = pygame.display.set_mode((800, 600))
     pygame.display.set_caption("Side ROLLER!")
+    
 
     roller = Roller()
     enemyR = redEnemy()
@@ -170,19 +200,24 @@ def game():
     enemyW = whiteEnemy()
     background = Background()
     bullet = Bullet()
+    obstical = Obstical()
+
     scoreboard = Scoreboard()
     
+    roller.soundBackground.play(-1)
+	
     backgroundSprite = pygame.sprite.Group(background)
     badSprites = pygame.sprite.Group(enemyR, enemyP, enemyW)
     scoreSprite = pygame.sprite.Group(scoreboard)
     playerSprite = pygame.sprite.Group(roller)
     bulletSprite = pygame.sprite.Group(bullet)
+    obsticalSprite = pygame.sprite.Group()
 	
     clock = pygame.time.Clock()
     keepGoing = True
+    pygame.mouse.set_visible(False)
     while keepGoing:
         clock.tick(30)
-        pygame.mouse.set_visible(False)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 keepGoing = False
@@ -192,88 +227,90 @@ def game():
                     bullet.shoot = True
 		
         hitEnemys = pygame.sprite.spritecollide(roller, badSprites, False)
-		
         if hitEnemys:
             roller.soundDeath.play()
             scoreboard.lives -= 1
             if scoreboard.lives <= 0:
-                print('Game over!')
+                keepGoing = False
                 scoreboard.lives = 5
                 scoreboard.score = 0
-            for theEnemy in killEnemys:
+            for theEnemy in hitEnemys:
                 theEnemy.reset()
 
+        hitObstical = pygame.sprite.spritecollide(roller, obsticalSprite, False)
+        if hitObstical:
+            roller.soundDeath.play()
+            scoreboard.lives -= 1
+            if scoreboard.lives <= 0:
+                keepGoing = False
+                scoreboard.lives = 5
+                scoreboard.score = 0
+            for theEnemy in hitObstical:
+                theEnemy.reset()
+				
         killEnemys = pygame.sprite.spritecollide(bullet, badSprites, False)
-
         if killEnemys:
             roller.soundKill.play()
             scoreboard.score += 100
             bullet.shoot = False
+            speedUp = False
+            if scoreboard.score % 600 == 0 and scoreboard.score != 0:
+                speedUp = True
+                if speedUp == True:
+                    speedUp = False
+                    enemyR.dx = enemyR.dx + 1
+                    enemyP.dx = enemyP.dx + 1
+                    enemyW.dx = enemyW.dx + 1
+                    print (enemyR.dx)
+                    print (enemyP.dx)
+                    print (enemyW.dx)
+                    print (scoreboard.score)
+            if scoreboard.score % 500 == 0 and scoreboard.score != 0:
+                obsticalSprite.add(obstical)
+                obstical.attack = True
+                print (obsticalSprite)
             for theEnemy in killEnemys:
                 theEnemy.reset()
-
-        previousScore = 0
-        if previousScore + 600 == scoreboard.score:
-            previousScore = scoreboard.score
-            enemyR.dx += 1
-            enemyP.dx += 1
-            enemyW.dx += 1
-            print (previousScore)
-            print (scoreboard.score)
-
 			
         backgroundSprite.update()
         badSprites.update()
         scoreSprite.update()
         bulletSprite.update()
         playerSprite.update()
-
+        obsticalSprite.update()
+		
         backgroundSprite.draw(screen)
         badSprites.draw(screen)
         scoreSprite.draw(screen)
         bulletSprite.draw(screen)
         playerSprite.draw(screen)
-        
+        obsticalSprite.draw(screen)
         
         
         pygame.display.flip()
-    
+   
     roller.soundBackground.stop()
+    roller.soundWaka.stop()
+    roller.soundDeath.stop()
+    roller.soundKill.stop()
+
     #return mouse cursor
     pygame.mouse.set_visible(True)
     return scoreboard.score
 	
-def instructions(score):
+def startScreen(score):
     roller = Roller()
-    background = Background()
+    intro_image = pygame.image.load("Menu-DRAW.png").convert()
+
+    pygame.display.set_caption("Intro")
     
-    allSprites = pygame.sprite.OrderedUpdates(background, roller)
-    insFont = pygame.font.SysFont(None, 50)
-
-    instructions = (
-    "Mail Pilot.     Last score: %d" % score ,
-    "Instructions:  You are a mail pilot,",
-    "delivering mail to the islands.",
-    "",
-    "Fly over an island to drop the mail,",
-    "but be careful not to fly too close",    
-    "to the clouds. Your plane will fall ",
-    "apart if it is hit by lightning too",
-    "many times.",
-    "",
-    "good luck!",
-    "",
-    "click to start, escape to quit..."
-    )
-
-    insLabels = []    
-    for line in instructions:
-        tempLabel = insFont.render(line, 1, (255, 255, 0))
-        insLabels.append(tempLabel)
+    roller.soundIntro.play(-1)
+	
+    screen.blit(intro_image, [0,0])
  
     keepGoing = True
     clock = pygame.time.Clock()
-    pygame.mouse.set_visible(False)
+    pygame.mouse.set_visible(True)
     while keepGoing:
         clock.tick(30)
         for event in pygame.event.get():
@@ -288,24 +325,56 @@ def instructions(score):
                     keepGoing = False
                     donePlaying = True
     
-        allSprites.update()
-        allSprites.draw(screen)
-
-        for i in range(len(insLabels)):
-            screen.blit(insLabels[i], (50, 30*i))
 
         pygame.display.flip()
-        
+    
+    roller.soundIntro.stop()
+    
     pygame.mouse.set_visible(True)
     return donePlaying
-        
+
+def gameOver(score):
+    roller = Roller()
+    intro_image = pygame.image.load("gameOver.png").convert()
+
+    pygame.display.set_caption("Game Over!")
+    
+    roller.soundIntro.play(-1)
+	
+    screen.blit(intro_image, [0,0])
+ 
+    keepGoing = True
+    clock = pygame.time.Clock()
+    pygame.mouse.set_visible(True)
+    while keepGoing:
+        clock.tick(30)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                keepGoing = False
+                donePlaying = True
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                keepGoing = False
+                donePlaying = False
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    keepGoing = False
+                    donePlaying = True
+    
+
+        pygame.display.flip()
+    
+    roller.soundIntro.stop()
+    
+    pygame.mouse.set_visible(True)
+    return donePlaying
+	
 def main():
     donePlaying = False
     score = 0
     while not donePlaying:
-        donePlaying = instructions(score)
+        donePlaying = startScreen(score)
         if not donePlaying:
             score = game()
-	
+            donePlaying = gameOver(score)
 if __name__ == "__main__":
     main()
